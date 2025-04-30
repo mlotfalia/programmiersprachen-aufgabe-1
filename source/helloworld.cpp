4ae730628d73b8ab4f75783cc852bf950d3b8b5c
Mahsa <mahsa.lotfalia@gmail.com>
2025-04-30 11:45:01 +0200

    Fix comparison with modulo operation

--- source/helloworld.cpp
+++ source/helloworld.cpp
@@ -2,6 +2,26 @@
 
 int main()
 {
-  std::cout << "Wellcome to the C++ toturial :) \n";
+  long long result = 1;
+
+  for (int i = 1; i <= 20; ++i) {
+      // Inline GCD computation using the Euclidean algorithm:
+      long long a = result;
+      long long b = i;
+      while (b != 0) {
+          long long temp = b;
+          b = a % b;
+          a = temp;
+      }
+      long long gcd = a;  // when b==0, a is the GCD
+
+      // Inline LCM update:
+      // lcm(result, i) = (result / gcd) * i
+      result = (result / gcd) * i;
+  }
+
+  std::cout << "Smallest number divisible by 1..20 is "
+            << result << "\n";
+
   return 0;
 }
